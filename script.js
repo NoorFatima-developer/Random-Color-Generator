45f747ebb65c4e9c5fd0acae8084af0014f5ff6c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-06 22:21:11 +0500

    code is added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const getColor = () => {
     const randomNumber = Math.floor(Math.random() * 16777215)
     const randomCode = "#" + randomNumber.toString(16);
     console.log(randomNumber, randomCode);
+    document.body.style.backgroundColor = randomCode;
     
     
 }
